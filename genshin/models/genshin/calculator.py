32c519dabe995c98e0764569c571e868a2b0b007
seria <seria.ati@gmail.com>
2025-10-04 15:49:29 +0900

    New models

--- genshin/models/genshin/calculator.py
+++ genshin/models/genshin/calculator.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import collections
+import enum
 import typing
 
 import pydantic
@@ -53,10 +54,30 @@ CALCULATOR_ARTIFACTS: typing.Mapping[int, str] = {
 }
 
 
+class MaterialSource(str, enum.Enum):
+    """Source of the material."""
+
+    SPECIAL = "MT_SPECIAL"
+    MONSTER = "MT_MONSTER"
+    WORLD = "MT_WORLD"
+    DOMAIN = "MT_DUNGEON"
+
+
+class PartialCalculatorCharacter(APIModel):
+    id: int
+    icon: str
+    rarity: int = Aliased("avatar_level")
+
+
+class PartialCalculatorWeapon(APIModel):
+    id: int
+    icon: str
+    rarity: int = Aliased("weapon_level")
+
+
 class CalculatorCharacter(character.BaseCharacter):
     """Character meant to be used with calculators."""
 
-    rarity: int = Aliased("avatar_level")
     element: str = Aliased("element_attr_id")
     weapon_type: str = Aliased("weapon_cat_id")
     level: int = Aliased("level_current", default=0)
@@ -222,18 +243,82 @@ class CalculatorArtifactResult(APIModel):
     list: typing.Sequence[CalculatorConsumable] = Aliased("id_consume_list")
 
 
+class CalculatorTalentInfo(APIModel):
+    """Information about a talent in the calculator."""
+
+    talent_id: int = Aliased("id")
+    current: int = Aliased("level_current", default=0)
+    target: int = Aliased("level_target", default=0)
+
+
+class CalculatorTalentResult(APIModel):
+    """Calculation result for a specific talent."""
+
+    info: CalculatorTalentInfo = Aliased("skill_info")
+    list: typing.Sequence[CalculatorConsumable] = Aliased("id_consume_list")
+
+
+class CalculatorMonster(APIModel):
+    """Monster that drops a material."""
+
+    id: int = Aliased("monster_id")
+    name: str = Aliased("monster_name")
+    icon: str = Aliased("monster_icon")
+    map_url: str = Aliased("monster_map_url")
+
+
+class CalculatorDomain(APIModel):
+    """Domain that drops a material."""
+
+    name: str = Aliased("dungeon_name")
+    weekdays: typing.Sequence[int] = Aliased("drop_day")
+    """List of weekdays the domain is open, 0=Monday, 6=Sunday."""
+    calendar_url: str = Aliased("calendar_link")
+
+
+class RemainingMaterialDetail(APIModel):
+    """Details of remaining required materials for a character."""
+
+    materials: typing.Sequence[CalculatorConsumable] = Aliased("consume")
+
+    characters: typing.Sequence[PartialCalculatorCharacter] = Aliased("avatars")
+    """Characters these materials are needed for, empty if accessing weapons."""
+    weapons: typing.Sequence[PartialCalculatorWeapon] = Aliased("weapons")
+    """Weapons these materials are needed for, empty if accessing characters or talents."""
+
+    material_source: MaterialSource
+
+    monster: typing.Optional[CalculatorMonster] = None
+    """Monster info, present if material_source is MT_MONSTER."""
+    map_url: typing.Optional[str] = None
+    """URL to interactive map, present if material_source is MT_WORLD."""
+    domain: typing.Optional[CalculatorDomain] = Aliased("dungeon_calendar", default=None)
+    """Domain info, may be present if material_source is MT_DUNGEON."""
+
+
+class RemainingMaterialDetails(APIModel):
+    """Details of remaining required materials for a character."""
+
+    characters: typing.Sequence[RemainingMaterialDetail] = Aliased("avatar_consume")
+    weapon: typing.Sequence[RemainingMaterialDetail] = Aliased("weapon_consume")
+    talents: typing.Sequence[RemainingMaterialDetail] = Aliased("avatar_skill_consume")
+
+
 class CalculatorResult(APIModel):
     """Calculation result."""
 
     character: list[CalculatorConsumable] = Aliased("avatar_consume")
     weapon: list[CalculatorConsumable] = Aliased("weapon_consume")
-    talents: list[CalculatorConsumable] = Aliased("avatar_skill_consume")
+    talents: list[CalculatorTalentResult] = Aliased("skills_consume")
     artifacts: list[CalculatorArtifactResult] = Aliased("reliquary_consume")
+
+    lineup_recommendation: typing.Optional[str] = Aliased("lineup_recommend", default=None)
 
     @property
     def total(self) -> typing.Sequence[CalculatorConsumable]:
+        talents = [i for t in self.talents for i in t.list]
         artifacts = [i for a in self.artifacts for i in a.list]
-        combined = self.character + self.weapon + self.talents + artifacts
+        combined = self.character + self.weapon + talents + artifacts
 
         grouped: dict[int, list[CalculatorConsumable]] = collections.defaultdict(list)
         for i in combined:
@@ -260,7 +345,15 @@ class CalculatorBatchResult(APIModel):
 
     characters: list[CalculatorResult] = Aliased("items")
     available_materials: list[CalculatorConsumable] = Aliased("available_material")
-    all_materials: list[CalculatorConsumable] = Aliased("overall_consume")
+    total_materials: list[CalculatorConsumable] = Aliased("overall_consume")
+    total_material_details: RemainingMaterialDetails = Aliased("overall_material_consume")
+    individual_results: typing.Sequence[CalculatorBatchResult] = Aliased("single_role_result")
+
+    has_user_info: bool
+
+    @property
+    def total_remaining_required_materials(self) -> typing.Sequence[CalculatorConsumable]:
+        return [m for m in self.total_materials if m.lacking > 0]
 
 
 class CalculatorFurnishingResults(APIModel):
